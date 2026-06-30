187868dfb291f9aa0a8eb8a946f27fe7dca77cae
Hendrik Kaiber <hendrik.b.kaiber@gmail.com>
2026-06-30 08:54:28 -0300

    Adicionar funcionalidade para converter NFSe

--- Python/utils/converter_nota_pdf.py
+++ Python/utils/converter_nota_pdf.py
@@ -1,10 +1,7 @@
 from brazilfiscalreport.danfe import Danfe
-import os
-import sys
-import chardet
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(parent_dir)
+from brazilfiscalreport.danfse import Danfse
 from limpar_arquivo import limpar_arquivo
+import os
 
 def converter_nota_pdf(xml_file):
 
@@ -18,5 +15,20 @@ def converter_nota_pdf(xml_file):
     danfe = Danfe(xml=content)
     danfe.output(filename)
 
-if __name__ == "__main__":
-    converter_nota_pdf("43260524660863000153550020000038921000064184.xml")
+def converter_nfse_pdf(xml_file):
+
+    filename = xml_file.replace('xml', 'pdf')
+
+    limpar_arquivo(xml_file)
+
+    with open(xml_file, 'r', encoding='utf-8', errors='replace') as file:
+        content = file.read()
+
+    danfe = Danfse(xml=content)
+    danfe.output(filename)
+
+if __name__ == '__main__':
+    files = os.listdir(os.getcwd())
+    for file in files:
+        if file.endswith(".xml"):
+            converter_nfse_pdf(file)
